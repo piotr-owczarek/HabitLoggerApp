e11834ecc9b42cc17d833dfd4850b3b849f71f20
szejku11 <piotrek.zwd@gmail.com>
2023-11-21 14:17:40 +0100

    feat: add model, database connection and operations, console ui

---
+++ DatabaseLibrary/Models/DrinkingWaterModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DatabaseLibrary.Models
+{
+    public class DrinkingWaterModel
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Quantity { get; set; }
+    }
+}
--- HabitLogger/Program.cs
+++ HabitLogger/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HabitLogger.Database;
+using HabitLogger.UI;
+
+DatabaseHelper.InitializeConnection();
+UserInterface.Start();
