21926fbf72e41295193e1006a8639411945b4fb3
szejku11 <piotrek.zwd@gmail.com>
2023-11-22 00:21:01 +0100

    fix: delete DatbaseLibrary folder

--- DatabaseLibrary/Models/DrinkingWaterModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DatabaseLibrary.Models
-{
-    public class DrinkingWaterModel
-    {
-        public int Id { get; set; }
-        public DateTime Date { get; set; }
-        public int Quantity { get; set; }
-    }
-}
